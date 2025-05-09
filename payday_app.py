2a6a13a2899a24e0da80c4100440a9208c889724
bgmgroup <bmdehan@gmail.com>
2025-05-09 04:59:23 -0400

    Update payday_app.py

--- payday_app.py
+++ payday_app.py
@@ -72,4 +72,7 @@ def invoice():
     return render_template('form.html', today=today, due=due)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
